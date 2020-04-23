74ae305dd9e7cc334f82b200ccf5f188dbe607bd
Prescilla Lecurieux <prescilla.lecurieux@gmail.com>
2020-04-23 08:25:58 +0200

    add crud controllers

---
+++ src/services/excelToJson.service.ts
@@ -0,0 +1,17 @@
+const XLSX = require('xlsx');
+
+export class excelToJsonService {
+    private static instance: excelToJsonService;
+
+    public static getInstance(): excelToJsonService {
+        if(!excelToJsonService.instance) {
+            excelToJsonService.instance = new excelToJsonService();
+        }
+        return excelToJsonService.instance;
+    }
+
+    public processXlsxToJson(filename = 'Base-Projet-IT-v3.xlsx'): void {
+        const workbook = XLSX.readFile(filename);
+        return XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]);
+    }
+}
