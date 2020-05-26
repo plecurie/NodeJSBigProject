a828a1a339e6adcf1c2a57d9759ed55e3b1a511c
Prescilla Lecurieux <prescilla.lecurieux@gmail.com>
2020-05-26 21:14:14 +0200

    attach network to both app & es

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,3 @@
-export const APP_PORT = process.env.PORT || 3100;
+export const APP_PORT = process.env.NODE_PORT || 3100;
 export const APP_HOST = "0.0.0.0";
 export const ES_URL = process.env.ELASTIC_URL || "http://localhost:9200";
